0cfe4e5a23dafb5c376ec85a133e8248ebae9019
christian@accelerate <christian.calloway@iver.se>
2025-01-31 10:19:48 +0100

    Add totp-cli wrapper script

--- src/mfa.sh
+++ src/mfa.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-set -eu
+## Forwards a given mfa seed a totp token generator
+## which writes to /dev/fd/10:pasteboard by default
+## $
+set -euo pipefail
 
-pbpaste | ssh laptop "cat >/tmp/mfa"
+## fd
+[ -e /dev/fd/10 ] || exec 10> >(pbcopy)
+
+## env
+: ${1?req! mfa seed}
+
+## main
+logger -sp DEBUG -- "Enter" \
+	:: "seed=$( echo "$1" | cut -c 1-5 )"
+
+echo "$1" \
+	| totp-cli instant \
+	| tee /dev/fd/10
